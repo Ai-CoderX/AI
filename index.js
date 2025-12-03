97f27e8ec458c6f058ce3224059edb5f5a55a2a4
kingx670 <herokujawad1@gmail.com>
2025-12-03 18:33:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -505,24 +505,17 @@ conn.ev.on('group-participants.update', async (update) => {
   });
 
 
-  conn.ev.on("messages.upsert", async (mek) => {
-    mek = mek.messages[0];
+  conn.ev.on("messages.upsert", async (event) => {
+    const mek = event.messages[0];
     if (!mek.message) return;
     mek.message =
       getContentType(mek.message) === "ephemeralMessage"
         ? mek.message.ephemeralMessage.message
         : mek.message;
 
-    // Message cache removed
-
     if (config.READ_MESSAGE === "true") {
       await conn.readMessages([mek.key]);
     }
-    if (mek.message.viewOnceMessageV2)
-      mek.message =
-        getContentType(mek.message) === "ephemeralMessage"
-          ? mek.message.ephemeralMessage.message
-          : mek.message;
     if (mek.key && mek.key.remoteJid === "status@broadcast" && config.AUTO_STATUS_SEEN === "true") {
       await conn.readMessages([mek.key]);
     }
