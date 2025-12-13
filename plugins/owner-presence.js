d976ce0b88f75c719b25eae570b15c8b67ab2ae6
kingx670 <herokujawad1@gmail.com>
2025-12-13 12:09:02 +0500

    Update and rename auto-recoding.js to owner-presence.js

---
+++ plugins/owner-presence.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('../config');
+const { cmd, commands } = require('../command');
+
+// Auto Typing Presence
+cmd({
+    on: "body"
+}, async (conn, mek, m, { from, body, isOwner, isGroup }) => {
+    if (config.AUTO_TYPING === 'true') {
+        // Work for both inbox and groups
+        await conn.sendPresenceUpdate('composing', from);
+    } 
+    else if (config.AUTO_TYPING === 'ib') {
+        // Work only for inbox (private chats)
+        if (!isGroup) {
+            await conn.sendPresenceUpdate('composing', from);
+        }
+    }
+    else if (config.AUTO_TYPING === 'group') {
+        // Work only for groups
+        if (isGroup) {
+            await conn.sendPresenceUpdate('composing', from);
+        }
+    }
+    // Any other value (including 'false') will disable auto typing
+});
+
+// Auto Recording Presence
+cmd({
+    on: "body"
+}, async (conn, mek, m, { from, body, isOwner, isGroup }) => {       
+    if (config.AUTO_RECORDING === 'true') {
+        // Work for both inbox and groups
+        await conn.sendPresenceUpdate('recording', from);
+    }
+    else if (config.AUTO_RECORDING === 'ib') {
+        // Work only for inbox (private chats)
+        if (!isGroup) {
+            await conn.sendPresenceUpdate('recording', from);
+        }
+    }
+    else if (config.AUTO_RECORDING === 'group') {
+        // Work only for groups
+        if (isGroup) {
+            await conn.sendPresenceUpdate('recording', from);
+        }
+    }
+    // Any other value (including 'false') will disable auto recording
+});
